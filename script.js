f43231a684e894bb06ca330b334b6a3b1d9008f3
Víctor Franco <victorfranco5@outlook.com>
2022-06-01 20:57:36 -0300

    feat: header and footer ok

--- script.js
+++
@@ -1,9 +0,0 @@
-const btnMenu = document.getElementById("teste");
-
-btnMenu.addEventListenner('click', botaClasse);
-
-function botaClasse(){
-    btnMenu.classList.toggle("testes");
-    alert("Hello!")
-}
-
